ac8f318523133172824c09b87f98badc6abcd47f
Ualid <ualid.khalid@pacemaker.ai>
2025-04-15 10:35:23 +0200

    Test

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static void testo() {
+        System.out.println("Test method");
+    }
+    
+}
