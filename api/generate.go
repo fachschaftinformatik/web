592ef73e198fcb5d0304f0b21ccde29f2569e772
Francesco La Camera <fm@lacamera.org>
2025-10-05 23:15:35 +0200

    feat(api): add users and authentication routes

--- api/generate.go
+++ api/generate.go
@@ -1,3 +1,5 @@
 package api
 
-//go:generate go tool oapi-codegen -config codegen.yaml openapi.yaml
+//go:generate go tool oapi-codegen -config codegen.types.yaml openapi.yaml
+//go:generate go tool oapi-codegen -config codegen.api.yaml openapi.yaml
+
